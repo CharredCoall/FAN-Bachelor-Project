e8fe0486f39c3d566db1c8c91e95f4741964bb58
Franja <toezzepige@hotmail.com>
2026-04-26 23:02:07 +0200

    Can check history now
    You can go back to previous chats, and hopefully the interactions between sending messages and also changing chats, should work. Also added loading visuals and fixed some other visual stuff

--- wasteful-fellas/scripts/send_message.gd
+++ wasteful-fellas/scripts/send_message.gd
@@ -13,35 +13,50 @@ extends TextEdit
 @onready var SFX = $AudioStreamPlayer
 @onready var init_chat_button = get_parent().get_node("StartConvo")
 
+@onready var ball_scene = preload("res://scripts/scenes/balls.tscn")
+
 var base_url = "https://wastefulfellas-dkdsd4bkhrepf8ec.swedencentral-01.azurewebsites.net"
 
-var cur_chat
+var cur_chat #chat button that appears
 
 var model_idx = 0
 var steps
 var log = []
 var last_sent_message = ""
 
+var history = {}
+var selected_chat #chat for history
+
 signal convo_started
 signal convo_ended
 
 func _ready():
+	set("theme_override_colors/font_placeholder_color", Color(0.348, 0.345, 0.33, 0.522))
 	cur_chat = init_chat_button
 	$HTTPRequest.request_completed.connect(self._request_completed)
+	
+	for i in range(5):
+		history[str(i)] = []
+	
+	print(history)
 
 func SendServerMessage() -> void:
+	placeholder_text = "Client is typing..."
+	set("theme_override_colors/font_placeholder_color", Color(0.348, 0.345, 0.33, 0.522))
 	#make green bubble
-	var green_bub = green_bubble.instantiate()
-	green_bub.prose = text
-	vcontainer.add_child(green_bub)
+	if selected_chat == globals.current_char:
+		var green_bub = green_bubble.instantiate()
+		green_bub.prose = text
+		vcontainer.add_child(green_bub)
+	#else add vfx at button?
+	
+	if text != null:
+		history[str(globals.current_char)].append(text)
 	
 	SFX.stop()
 	SFX.volume_db = 10.0
 	SFX.stream = load("res://art/SFXs/plop_sfx.mp3") 
 	SFX.play()
-	
-	#green bubble for now!
-	#$Timer.start()
 	
 	editable = false
 	
@@ -56,7 +71,18 @@ func SendServerMessage() -> void:
 		
 	clear()
 	
+	var balls = ball_scene.instantiate()
+	balls.get_child(-1).play("ball_bounce")
+	balls.name = "BALLS"
+	vcontainer.add_child(balls)
+	
+	
 func _request_completed(_result, _response_code, _headers, body):
+	placeholder_text = "Write something..."
+	set("theme_override_colors/font_placeholder_color", Color(0.0, 0.425, 0.593, 0.522))
+	if vcontainer.get_node("BALLS") != null:
+		vcontainer.get_node("BALLS").queue_free()
+	
 	var json = JSON.new()
 	json.parse(body.get_string_from_utf8())
 	var dict_package = json.get_data()
@@ -85,10 +111,11 @@ func _request_completed(_result, _response_code, _headers, body):
 	if "steps" in dict_package:
 		steps = dict_package["steps"]
 	
-	var blue_bub = blue_bubble.instantiate()
-	blue_bub.prose = str(dict_package["response"])
-	print("Response:", dict_package["response"], "\n")
-	vcontainer.add_child(blue_bub)
+	if selected_chat == globals.current_char:
+		var blue_bub = blue_bubble.instantiate()
+		blue_bub.prose = str(dict_package["response"])
+		print("Response:", dict_package["response"], "\n")
+		vcontainer.add_child(blue_bub)
 	
 	SFX.stop()
 	SFX.volume_db = -10.0
@@ -96,9 +123,10 @@ func _request_completed(_result, _response_code, _headers, body):
 	SFX.play()
 	clear() #clear message!
 
-	editable = true
+	if selected_chat == globals.current_char:
+		editable = true
 	
-	$"../Button".disabled = false
+		$"../Button".disabled = false
 	
 	if len(log) == 0 or log == []:
 		last_sent_message = "[System Information: The fridge currently contains: " + str(GameManager.fridge) + "]
@@ -107,8 +135,13 @@ func _request_completed(_result, _response_code, _headers, body):
 	
 	log.append(['"' + last_sent_message.replace('"', "'") + '"', '"' + dict_package["response"].replace('"', "'") + '"'])
 	
+	history[str(globals.current_char)].append(dict_package["response"])
+	print(history)
+	
 	if "ended" in dict_package and dict_package["ended"]:
 		_end_convo()
+	
+	
 	
 
 func _on_text_changed() -> void:
@@ -166,37 +199,71 @@ func _end_convo():
 	
 	#$"../../Panel/StartConvo".disabled = false
 	
-	var end_label = Label.new()
-	end_label.text = "--- Conversation ended ---"
-	end_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	end_label.add_theme_color_override("font_color", Color.GRAY)
+	#var end_label = Label.new()
+	#end_label.text = "--- Conversation ended ---"
+	#end_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	#end_label.add_theme_color_override("font_color", Color.GRAY)
 	
 	log = []
   
-	vcontainer.add_child(end_label)
+	#vcontainer.add_child(end_label)
 	
 
 
-func _on_start_convo_pressed() -> void:
-	log = []
-	
-	print("Attempting to request message from character:", globals.current_char)
-	var body = JSON.stringify({"char_idx": int(globals.current_char)})
-	var error = $HTTPRequest.request(base_url + "/start_convo", ["Content-type: application/json"], HTTPClient.METHOD_GET, body)
-	if error != OK:
-		push_error("An error occurred in the HTTP request.")
-	
-	emit_signal("convo_started")
+func _on_start_convo_pressed(chat_idx) -> void:
+	selected_chat = chat_idx
+	if history[str(chat_idx)] != []:
+		load_history(chat_idx)
 		
-	$"../../../Client/Panel/EndConvoButton".disabled = false
+		if selected_chat != globals.current_char: #only be able to respond if in correct chat
+			editable = false
+		else:
+			editable = true
+		
+	else:
+		load_history(chat_idx)
+		log = []
+		
+		placeholder_text = "Client is typing..."
 	
-	cur_chat.disabled = true
+		print("Attempting to request message from character:", globals.current_char)
+		var body = JSON.stringify({"char_idx": int(globals.current_char)})
+		var error = $HTTPRequest.request(base_url + "/start_convo", ["Content-type: application/json"], HTTPClient.METHOD_GET, body)
+		if error != OK:
+			push_error("An error occurred in the HTTP request.")
 	
-	var start_label = Label.new()
-	start_label.text = "--- Conversation started ---"
-	start_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	#start_label.pos.y += 50
-	start_label.add_theme_color_override("font_color", Color.GRAY)
+		emit_signal("convo_started")
+		
+		$"../../../Client/Panel/EndConvoButton".disabled = false
 	
-	vcontainer.add_child(start_label)
+		cur_chat.disabled = false
+		
+		var balls = ball_scene.instantiate()
+		balls.get_child(-1).play("ball_bounce")
+		balls.name = "BALLS"
+		vcontainer.add_child(balls)
 	
+		#var start_label = Label.new()
+		#start_label.text = "\n--- Conversation started ---"
+		#start_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		#start_label.pos.y += 50
+		#start_label.add_theme_color_override("font_color", Color.GRAY)
+	
+		#vcontainer.add_child(start_label)
+		
+	
+
+func load_history(idx):
+	for child in vcontainer.get_children():
+		child.queue_free()
+		
+	var convo = history[str(idx)]
+	if convo != []:
+		for i in range(convo.size()):
+			var bubble
+			if i%2 == 0:
+				bubble = blue_bubble.instantiate()
+			else:
+				bubble = green_bubble.instantiate()
+			bubble.prose = convo[i]
+			vcontainer.add_child(bubble)
