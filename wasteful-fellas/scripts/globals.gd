f6e85b2e1b93af5ca4e2b7f8b0b57dd308aa5168
CharredCoall <104776594+CharredCoall@users.noreply.github.com>
2026-05-07 16:41:44 +0200

    Creates new Session id for every session

--- wasteful-fellas/scripts/globals.gd
+++ wasteful-fellas/scripts/globals.gd
@@ -1,7 +1,5 @@
 extends Node
 
-@onready var session_id = RandomNumberGenerator.new().randi_range(0, 999999999)
-
 #var max_window_index = 0
 
 var current_char = 0
@@ -11,3 +9,5 @@ var characters = ["Jim", "Brody", "Cecilia", "Jasmine", "Voll"]
 var max_points = 0.0
 
 var sound_on = true
+
+var session_id = 0
--- wasteful-fellas/scripts/scenes/login_screen.gd
+++ wasteful-fellas/scripts/scenes/login_screen.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _start_game():
+	globals.session_id = RandomNumberGenerator.new().randi_range(0, 999999999)
 	get_tree().change_scene_to_file("res://scripts/scenes/desktop.tscn")
