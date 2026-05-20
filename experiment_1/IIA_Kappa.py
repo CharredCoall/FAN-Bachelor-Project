875ab81c2b71586299865a0905da71b938c16503
CharredCoall <104776594+CharredCoall@users.noreply.github.com>
2026-05-20 16:42:25 +0200

    Added weighted version of iia score

--- experiment_1/IIA_Kappa.py
+++ experiment_1/IIA_Kappa.py
@@ -27,6 +27,41 @@ def fleiss_kappa(M):
 
     return round((Pbar - PbarE) / (1 - PbarE), 4)
 
+def weighted_kappa(M):
+
+    N, q = M.shape # N is # of items, q is # of categories
+    w = 1 - ((np.subtract.outer(np.arange(q),np.arange(q))) ** 2) / (q-1) ** 2 #Quadratic weights for the scale 0-2
+    r = np.sum(M, axis=1) # r is the number of raters that assigned item i to any category
+
+    r_star = np.zeros((N,q))
+    p_o_prime = 0
+    r_bar = np.mean(r)
+    epsilon = 1/(N*r_bar)
+    p_o = 0
+    pi = np.zeros(q)
+    p_c = 0
+
+    for i in range(N):
+        for k in range(q):
+            r_star[i,k] = np.dot(w[k], M[i])
+
+    for i in range(N):
+        for k in range(q):
+            p_o_prime += (M[i,k] * (r_star[i,k] - 1)) / (r_bar * (r[i] - 1))
+
+    p_o_prime /= N
+
+    p_o = p_o_prime * (1-epsilon) + epsilon    
+
+    pi = np.mean(M/r_bar,  axis=0)
+
+    for k in range(q):
+        for l in range(q):
+            p_c += w[k,l] * pi[k] * pi[l]
+
+
+    return round((p_o-p_c)/(1-p_c) ,4)
+
 
 
 def run_iia():
@@ -56,6 +91,7 @@ def run_iia():
     M = M[~np.all(M == 0, axis=1)]
 
     print(fleiss_kappa(M))
+    print(weighted_kappa(M))
 
 
 if __name__ == "__main__":
